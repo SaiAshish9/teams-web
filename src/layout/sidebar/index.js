93e4619568b39c2cc7deada70b29752e080a1a26
saiashish9 <saiashish3760@gmail.com>
2021-08-21 13:54:39 +0530

    sidebar updated

--- src/layout/sidebar/index.js
+++ src/layout/sidebar/index.js
@@ -1,9 +1,60 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Img, ListItem, ListItemLabel } from "./styles";
+
+import BellIcon from "assets/images/sidebar/bellIcon.svg";
+import ChatIcon from "assets/images/sidebar/chatIcon.svg";
+import TeamsIcon from "assets/images/sidebar/teamsIcon.svg";
+import AssignmentsIcon from "assets/images/sidebar/assignmentsIcon.svg";
+import CalendarIcon from "assets/images/sidebar/calendarIcon.svg";
+import CallIcon from "assets/images/sidebar/callIcon.svg";
+
+const icons = [
+  {
+    icon: BellIcon,
+    text: "Activity",
+  },
+  {
+    icon: ChatIcon,
+    text: "Chat",
+  },
+  {
+    icon: TeamsIcon,
+    text: "Teams",
+  },
+  {
+    icon: AssignmentsIcon,
+    text: "Assignments",
+  },
+  {
+    icon: CalendarIcon,
+    text: "Calendar",
+  },
+  {
+    icon: CallIcon,
+    text: "Calls",
+  },
+  {
+    icon: BellIcon,
+    text: "Apps",
+  },
+  {
+    icon: BellIcon,
+    text: "Help",
+  },
+];
 
 const Sidebar = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      {icons.slice(0, icons.length - 2).map((i, k) => (
+        <ListItem key={k}>
+          <Img src={i.icon} alt="img" />
+          <ListItemLabel>{i.text}</ListItemLabel>
+        </ListItem>
+      ))}
+    </Container>
+  );
 };
 
 export default Sidebar;
--- src/layout/sidebar/styles.js
+++ src/layout/sidebar/styles.js
@@ -3,5 +3,26 @@ import styled from "styled-components";
 export const Container = styled.div`
   height: calc(100% - 3rem);
   width: 4rem;
-  background: #333449;
+  background: #ebebeb;
+  box-shadow: 0 0 3px #999;
+`;
+
+export const Img = styled.img`
+  width: 54%;
+`;
+
+export const ListItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0.5rem 0;
+  &:hover {
+    background: #fff;
+  }
+`;
+
+export const ListItemLabel = styled.p`
+  font-size: 0.6rem;
+  color: #797979;
 `;
