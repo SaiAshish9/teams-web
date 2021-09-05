d6872cc04bcb118f0ecd8ab75b7284812285b626
saiashish9 <saiashish3760@gmail.com>
2021-09-06 02:01:54 +0530

    updated nav

--- src/layout/header/components/threeDots/styles.js
+++ src/layout/header/components/threeDots/styles.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin-right: 1rem;
+`;
 
 export const StyledImg = styled.img``;
