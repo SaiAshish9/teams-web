fffbe415f6265ffaa805db48c9f401858c8053a3
saiashish9 <saiashish3760@gmail.com>
2021-10-06 21:34:34 +0530

    updated home screen

--- src/screens/HomeScreen/components/mainContent/components/card/index.js
+++ src/screens/HomeScreen/components/mainContent/components/card/index.js
@@ -1,10 +1,31 @@
 import React, { useState } from "react";
 
-import { Container, Img, Label, Img1 } from "./styles";
+import {
+  Container,
+  Img,
+  Label,
+  Img1,
+  MenuContent,
+  Divider,
+  ListItem,
+  ListItemLabel,
+  ListItemImg,
+} from "./styles";
 
 import ThreeDotsDark from "assets/images/sidebar/threeDots-white.svg";
 import ThreeDotsFilled from "assets/images/sidebar/threeDots-filled.svg";
 import ThreeDots from "assets/images/sidebar/threeDots.svg";
+
+import HideSvg from "assets/images/dropdown/hide.svg";
+import HideSvgDark from "assets/images/dropdown/hideDark.svg";
+import TeamSvg from "assets/images/dropdown/team.svg";
+import TeamSvgDark from "assets/images/dropdown/teamDark.svg";
+import MemberSvg from "assets/images/dropdown/member.svg";
+import MemberSvgDark from "assets/images/dropdown/memberDark.svg";
+import LinkSvg from "assets/images/dropdown/link.svg";
+import LinkSvgDark from "assets/images/dropdown/linkDark.svg";
+import Tags from "assets/images/dropdown/tags.svg";
+import TagsDark from "assets/images/dropdown/tagsDark.svg";
 
 import { useStore } from "store";
 
@@ -17,10 +38,80 @@ const Card = ({ text, img }) => {
 
   const [hovered, setHovered] = useState(false);
 
+  const [clicked, setClicked] = useState();
+
+  const [selected, setSelected] = useState(-1);
+
+  function handleClick(e) {
+    e.preventDefault();
+    // alert(e.type);
+    setClicked((c) => !c);
+  }
+
+  const DATA = [
+    {
+      img:
+        theme === Theme.light
+          ? HideSvg
+          : theme === Theme.highContrast
+          ? selected === 0
+            ? HideSvg
+            : HideSvgDark
+          : HideSvgDark,
+      text: "Hide",
+    },
+    {
+      img:
+        theme === Theme.light
+          ? TeamSvg
+          : theme === Theme.highContrast
+          ? selected === 1
+            ? TeamSvg
+            : TeamSvgDark
+          : TeamSvgDark,
+      text: "Manage Team",
+    },
+    {
+      img:
+        theme === Theme.light
+          ? MemberSvg
+          : theme === Theme.highContrast
+          ? selected === 2
+            ? MemberSvg
+            : MemberSvgDark
+          : MemberSvgDark,
+      text: "Add Team",
+    },
+    {
+      img:
+        theme === Theme.light
+          ? LinkSvg
+          : theme === Theme.highContrast
+          ? selected === 3
+            ? LinkSvg
+            : LinkSvgDark
+          : LinkSvgDark,
+      text: "Get link to team",
+    },
+    {
+      img:
+        theme === Theme.light
+          ? Tags
+          : theme === Theme.highContrast
+          ? selected === 4
+            ? Tags
+            : TagsDark
+          : TagsDark,
+      text: "Manage tags",
+    },
+  ];
+
   return (
     <Container
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
+      // onClick={handleClick}
+      onContextMenu={handleClick}
     >
       <Img1
         src={
@@ -32,12 +123,38 @@ const Card = ({ text, img }) => {
             ? hovered
               ? ThreeDots
               : ThreeDotsDark
-            : ThreeDotsDark
+            : clicked
+            ? ThreeDotsFilled
+            : hovered
+            ? ThreeDotsDark
+            : ThreeDots
         }
         alt="img"
       />
       <Img src={img} alt="img" />
       <Label>{text}</Label>
+      {clicked && (
+        <MenuContent>
+          <ListItem
+            onMouseEnter={() => setSelected(0)}
+            onMouseLeave={() => setSelected(-1)}
+          >
+            <ListItemImg alt="img" src={DATA[0].img} />
+            <ListItemLabel>{DATA[0].text}</ListItemLabel>
+          </ListItem>
+          <Divider />
+          {DATA.slice(1).map((i, k) => (
+            <ListItem
+              key={k}
+              onMouseEnter={() => setSelected(k + 1)}
+              onMouseLeave={() => setSelected(-1)}
+            >
+              {i.img && <ListItemImg alt="img" src={i.img} />}
+              <ListItemLabel>{i.text}</ListItemLabel>
+            </ListItem>
+          ))}
+        </MenuContent>
+      )}
     </Container>
   );
 };
--- src/screens/HomeScreen/components/mainContent/components/card/styles.js
+++ src/screens/HomeScreen/components/mainContent/components/card/styles.js
@@ -2,8 +2,19 @@ import styled, { css } from "styled-components";
 
 import { COLORS, Styles, Theme } from "constants/index";
 
-const { ebb, mineShaft3, mineShaft2, gray, mineShaft4, yellow, gallery } =
-  COLORS;
+const {
+  ebb,
+  mineShaft3,
+  mineShaft2,
+  gray,
+  mineShaft4,
+  yellow,
+  gallery,
+  mineShaft,
+  desertStorm,
+  alto,
+  white,
+} = COLORS;
 
 export const Container = styled.div`
   height: 15rem;
@@ -71,4 +82,87 @@ export const Label = styled.p`
   height: 3rem;
   font-size: 1rem;
   text-align: center;
+`;
+
+export const MenuContent = styled.div`
+  width: 12rem;
+  background: #292929;
+  box-shadow: 0 0.2rem 1.6rem 0 rgb(0 0 0 / 50%);
+  border-radius: 0.4rem;
+  font-size: 0.9rem;
+  border: 0;
+  position: absolute;
+  top: 17%;
+  left: 86%;
+  z-index: 3;
+  padding: 0;
+  ${({ theme: { current } }) =>
+    current === Theme.highContrast &&
+    css`
+      background: #000;
+      border: 1px solid #fff;
+    `};
+  ${({ theme: { current } }) =>
+    current === Theme.light &&
+    css`
+      background: ${white};
+      box-shadow: 0 0.2rem 1.6rem 0 rgb(37 36 35 / 30%);
+      color: #000;
+    `};
+`;
+
+export const ListItem = styled.div`
+  color: #fff;
+  width: 100%;
+  font-weight: 300;
+  font-size: 0.72rem;
+  ${Styles.RBC};
+  justify-content: flex-start;
+  ${({ theme: { current } }) =>
+    current === Theme.dark &&
+    css`
+      color: ${alto};
+      &:hover {
+        background: ${mineShaft3};
+      }
+    `};
+  ${({ theme: { current } }) =>
+    current === Theme.highContrast &&
+    css`
+      &:hover {
+        background: ${yellow};
+        color: #000;
+      }
+    `};
+  ${({ theme: { current } }) =>
+    current === Theme.light &&
+    css`
+      color: #000;
+      &:hover {
+        background: ${ebb};
+      }
+    `}
+  padding: 0.27rem 0.6rem;
+`;
+
+export const Divider = styled.div`
+  height: 0.18rem;
+  background: ${desertStorm};
+  ${({ theme: { current } }) =>
+    current === Theme.dark &&
+    css`
+      background: ${mineShaft};
+    `}
+  ${({ theme: { current } }) =>
+    current === Theme.highContrast &&
+    css`
+      height: 0.04rem;
+      opacity: 0.4;
+    `}
+`;
+
+export const ListItemLabel = styled.p``;
+
+export const ListItemImg = styled.img`
+  width: 1.8rem;
 `;
