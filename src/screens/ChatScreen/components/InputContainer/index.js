d72b177775d52d7419c975f0ed7faa8b18f6201e
saiashish9 <saiashish3760@gmail.com>
2021-11-04 00:29:06 +0530

    header updated

--- src/screens/ChatScreen/components/InputContainer/index.js
+++ src/screens/ChatScreen/components/InputContainer/index.js
@@ -1,11 +1,44 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, EmojiCont, Input, Image } from "./styles";
+
+import Img1 from "./assets/img1";
+import Img2 from "./assets/img2";
+import Img3 from "./assets/img3";
+import Img4 from "./assets/img4";
+import Img5 from "./assets/img5";
+import Img6 from "./assets/img6";
+import Img7 from "./assets/img7";
+import Img8 from "./assets/img8";
+import Img9 from "./assets/img9";
+import Img10 from "./assets/img10";
+import Img11 from "./assets/img11";
+import Img12 from "./assets/img12";
+
+const data = [
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+  <Img1 color="#fff" />,
+];
 
 const InputContainer = () => {
   return (
     <Container>
-      <p>hi</p>
+      <Input placeholder="Type a new message" />
+      <EmojiCont>
+        {data.map((i, k) => (
+          <Image key={k}>{i} </Image>
+        ))}
+      </EmojiCont>
     </Container>
   );
 };
--- src/screens/ChatScreen/components/InputContainer/styles.js
+++ src/screens/ChatScreen/components/InputContainer/styles.js
@@ -1,7 +1,36 @@
 import styled, { css } from "styled-components";
 
+import { Styles, COLORS } from "constants/index";
+
+const { yellow } = COLORS;
+
 export const Container = styled.div`
-  width: 100%;
-  height: 2rem;
-  background: red;
+  width: 70%;
 `;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 2.4rem;
+  background: transparent;
+  ${Styles.HideBorder};
+  border: 1px solid #fff !important;
+  border-radius: 0.2rem;
+  padding: 0.5rem 0.8rem;
+  color: #fff;
+  &::placeholder {
+    color: #fff;
+    font-size: 0.8rem;
+    font-weight: 300;
+  }
+  &:focus {
+    border-bottom: 2px solid ${yellow} !important;
+  }
+`;
+
+export const EmojiCont = styled.div`
+  width: 100%;
+  height: 2.4rem;
+  ${Styles.RBC};
+`;
+
+export const Image = styled.div``;
