4c5543a945529f560f6905ad88603184ad96a6b6
saiashish9 <saiashish3760@gmail.com>
2021-11-07 01:44:02 +0530

    header updated

--- src/screens/NewMeeting/index.js
+++ src/screens/NewMeeting/index.js
@@ -1,8 +1,23 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useRef, useEffect } from "react";
+import { Container, Input, Label, VideoContainer, Tag, Button } from "./styles";
 
 const NewMeetingContainer = () => {
-  return <Container></Container>;
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <Container>
+      <Label>Choose your audio and video settings for</Label>
+      <Input ref={inputRef} sanitized defaultValue={`Meeting in "General" `} />
+      <VideoContainer>
+          <Button>Join now</Button>
+      </VideoContainer>
+      <Tag>Other join options</Tag>
+    </Container>
+  );
 };
 
 export default NewMeetingContainer;
--- src/screens/NewMeeting/styles.js
+++ src/screens/NewMeeting/styles.js
@@ -1,7 +1,59 @@
 import styled, { css } from "styled-components";
 
+import { Styles } from "constants/index";
+
 export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
   background: #000;
+  ${Styles.CCC};
+`;
+
+export const Label = styled.p`
+  color: #fff;
+  font-size: 0.8rem;
+  font-weight: 300;
+  text-align: center;
+`;
+
+export const Input = styled.input`
+  ${Styles.HideBorder};
+  border: 1px solid #fff !important;
+  border-radius: 0.2rem;
+  padding: 0 0.7rem !important;
+  line-height: normal;
+  background: #000;
+  margin-top: 0.5rem;
+  width: 428px;
+  font-size: 1.6rem;
+  height: 39px;
+  color: #fff;
+  text-align: center;
+`;
+
+export const VideoContainer = styled.div`
+  width: 570px;
+  height: 320px;
+  border: 2px solid #fff;
+  margin: 3.6rem auto 4rem;
+`;
+
+export const Tag = styled.p`
+  color: #fff;
+  font-size: 0.72rem;
+  font-weight: 500;
+  text-align: center;
+`;
+
+export const Button = styled.div`
+  border: 1px solid #fff;
+  //   min-width: 9.6rem;
+  width: fit-content;
+  padding: 0.4rem 2rem;
+  cursor: pointer;
+  color: #fff;
+  margin: 2rem auto;
+  &:hover {
+    background: yellow;
+    color: #000;
+  }
 `;
