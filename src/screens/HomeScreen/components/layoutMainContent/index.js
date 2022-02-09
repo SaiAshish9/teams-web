6c6248a7cdab8f9c640f0474f126c42cf5e9c8d8
saiashish9 <saiashish3760@gmail.com>
2022-02-09 22:40:06 +0530

    header updated

--- src/screens/HomeScreen/components/layoutMainContent/index.js
+++ src/screens/HomeScreen/components/layoutMainContent/index.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Container } from "./styles";
 import { Sidebar } from "./components";
 import { MainSection } from "screens/TeamScreen/components";
 import { TEAMS_DATA } from "constants/index";
-import { useState } from "react/cjs/react.development";
 
 const LayoutMainContent = () => {
   const [itemSelected, setItemSelected] = useState(0);
